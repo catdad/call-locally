ac9d175967533116883c07b4b2be49c1c810be1f
Kiril Vatev <vatev.1@gmail.com>
2016-06-29 20:22:58 -0400

    checking if current lib is already the local one
    #2
    
    totally didn't test any of this

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ var util = require('util');
 
 var BASE = process.cwd();
 
+function isInside(parent, child) {
+    var rel = path.relative(parent, child);
+    
+    if (/^\.\./.test(rel)) {
+        return false;
+    } else if (/^\//.test(rel)) {
+        return false;
+    } else if (/^[a-z]:/i.test(rel)) {
+        return false;
+    }
+    
+    return true;
+}
+
 /**
  * @param {object} opts The options object.
  * @param {string} opts.name The name of the module.
@@ -49,16 +63,29 @@ module.exports = function callLocally(opts, callback) {
         var resolvedUri = path.posix.join(BASE, 'node_modules', NAME, CLI);
         return tryRequire(resolvedUri);
     }
+    
+    // Checks if this is already the locally installed module, and if so,
+    // uses it.
+    function checkIfAlreadyLocal() {
+        var tentativeLocal = path.resolve(__dirname, '..', '..', CLI);
+        
+        if (isInside(BASE, tentativeLocal)) {
+            return tryRequire(tentativeLocal);
+        }
+        
+        return false;
+    }
 
     // We did not find grandma installed locally, nor are we
     // executing in the grandma folder itself (which might happen
     // during development), so we will use the global one instead.
     function continueWithCurrent() {
+        // TODO cannot assume this
         var resolvedUri = './cli.js';
         return tryRequire(resolvedUri);
     }
 
-    var tasks = [ requireLocal ];
+    var tasks = [ requireLocal, checkIfAlreadyLocal ];
     
     if (opts.allowGlobal === true) {
         tasks.push(continueWithCurrent);
